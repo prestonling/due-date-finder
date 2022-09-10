6dbb0f248e30765bd18611f36b95817daa880527
thestungyer <47674413+thestungyer@users.noreply.github.com>
2022-09-10 11:34:32 -0600

    Update App.js

--- due-date-finder/src/App.js
+++ due-date-finder/src/App.js
@@ -2,34 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
-function FileUploadPage(){
-  return(
-    <div>
-      <input type="file" name="file" onChange={changeHandler} />
-      <div>
-        <button onClick={handleSubmission}>Submit</button> </div>
-          </div>
-   )
-  }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <input type="file" name="file" />
+    <div>
+      <button>Submit</button>
     </div>
+  </div>
   );
 }
 
