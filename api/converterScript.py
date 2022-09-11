94892010e63b69412973b62007e50972fe85e7c7
adhanani90 <77396180+adhanani90@users.noreply.github.com>
2022-09-11 11:20:31 -0600

    Made the convertScript more general

--- api/converterScript.py
+++ api/converterScript.py
@@ -1,19 +1,25 @@
-"""
-import pdfplumber
+def is_date(string, fuzzy=False):
+    """
+    This is not my function. found this code here : https://stackoverflow.com/questions/25341945/check-if-string-has-date-any-format
+    Return whether the string can be interpreted as a date.
 
-syllabus = ""
-with pdfplumber.open(r'Syllabus_MGTSC405_F2022_08Sep2022.pdf') as pdf:
-    for page in pdf.pages:
-        syllabus += page.extract_text()
+    :param string: str, string to check for date
+    :param fuzzy: bool, ignore unknown tokens in string if True
+    """
+    try: 
+        parse(string, fuzzy=fuzzy)
+        return True
 
-print(syllabus)
-
-"""
+    except ValueError:
+        return False
+from lib2to3.pgen2.parse import ParseError
 from time import strftime
 from tabula import read_pdf
 from tabulate import  tabulate
 from dateutil.parser import parse
 import csv
+from dateutil import parser
+
 from dateutil.parser import parse
 from icalendar import Calendar, Event, vCalAddress, vText
 import pytz
@@ -21,6 +27,9 @@ from datetime import datetime
 import os
 from pathlib import Path
 import camelot
+from dateutil.parser import parse
+
+#filepath = r"C:\Users\dhana\Documents\Hackathon\Syllabus_MGTSC405_F2022_08Sep2022.pdf"
 
 filename = "uploads/Syllabus_MGTSC405_F2022_08Sep2022.pdf"
 dirname = os.path.dirname(__file__)
@@ -29,38 +38,34 @@ df = read_pdf(
     filepath, pages="all", encoding='mac_roman'
 )
 
-#for table in df:
- #   print(table)
-    #table.to_csv("Table.csv", encoding='utf-8', index=False)
-
-table = df[0].values.tolist()
-
-print(table)
-
-
-date_string = table[0][1]
-
-
-
-year = str(2022)
-
-print("date_string =", date_string)
-print("type of date_string =", type(date_string))
-
 cal = Calendar()
 
-for i in range(len(table)-1): 
-    event = Event()
-    event.add('summary', table[i][0])
-    date_string = table[i][1]
-    date_string = year + ", " +date_string
-    the_date = datetime.strptime(date_string, "%Y, %B %d")
-    print(the_date)
-    event.add('dtstart', the_date)   # YYYY,MM,DD
-    cal.add_component(event)
+table_index = -1
+date_index = -1
+other_index = -1
+for tables in df:
+    tables = tables.values.tolist()
+    for rows in tables:
+        for entries in rows:
+            try:   
+                if is_date(entries):
+                    
+                    #table_index = df.index('tables')
+                    date_index = rows.index(entries)
+                    event = Event()
+                    summary_index = 1 - date_index
+                    summary = rows[summary_index]
+                    date = parser.parse(rows[date_index])
+                    event.add('summary',summary)
+                    event.add('dtstart', date)   
+                    print(date)
+                    cal.add_component(event)
 
+            except TypeError:
+                continue
+  
 directory = str(Path(__file__).parent.parent) + "/"
 print("ics file will be generated at ", directory)
 f = open(os.path.join(directory, 'example.ics'), 'wb')
 f.write(cal.to_ical())
-f.close()
+f.close()        
